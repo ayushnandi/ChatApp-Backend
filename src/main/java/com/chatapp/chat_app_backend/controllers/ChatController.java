f9d3b2662177a77bc978d1505139ea57d645ab2e
unknown <ayushnandi>
2025-01-30 12:50:26 +0530

    Intial commit

--- src/main/java/com/chatapp/chat_app_backend/controllers/ChatController.java
+++ src/main/java/com/chatapp/chat_app_backend/controllers/ChatController.java
@@ -1,4 +1,24 @@
 package com.chatapp.chat_app_backend.controllers;
 
+
+import com.chatapp.chat_app_backend.entities.Message;
+import com.chatapp.chat_app_backend.reopsitories.RoomReopsitory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatController {
+
+
+    private RoomReopsitory roomReopsitory;
+
+    public ChatController (RoomReopsitory roomReopsitory){
+        this.roomReopsitory = roomReopsitory;
+    }
+
+    @MessageMapping("/sendMessage/{roomId}")
+    public Message sendMessage(){
+
+    }
+
 }
